d743948b51f1995ebbc072ee0b6173b08d6a7914
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2023-08-31 10:19:40 +0100

    Privacy Polocy (Apparently I need onee)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import "./styles/index.css";
 import Home from "./pages/Home";
 import ABQD from "./pages/ABQD";
+import QDPP from "./pages/QDPP";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/QuickDraw" element={<ABQD />} />
+        <Route path="/QuickDraw/PP" element={<QDPP />} />
       </Routes>
     </HashRouter>
   );
---
+++ src/pages/QDPP.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QDPP = () => {
+  return (
+    <div>
+      We do not use or gather any data about the user when using this game.
+    </div>
+  );
+};
+
+export default QDPP;
