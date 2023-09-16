ce82c820318b61bb01763e73ddbdd95146045e17
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2023-09-16 12:40:06 +0100

    Move stuff about

--- src/App.js
+++ src/App.js
@@ -5,25 +5,10 @@ import ABQD from "./pages/ABQD";
 import QDPP from "./pages/QDPP";
 import QDTC from "./pages/QDTC";
 import QDGT from "./pages/QDGT";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      window.dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
-
-    gtag("config", "G-Q36G4C2FEX");
-  }, []);
-
   return (
     <HashRouter>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-Q36G4C2FEX"
-      ></script>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/QuickDraw" element={<ABQD />} />
