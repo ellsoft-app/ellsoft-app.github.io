6ce2eb283cafbc0335c45b8fd5ed9c632fa14874
TutorialMan7727 <157202119+TutorialMan7727@users.noreply.github.com>
2024-12-19 16:04:22 +0000

    Autotok

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from "./Home";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import QuickDraw from "./QD/QuickDraw";
+import Autotok from "./Autotok/Autotok";
 import QuickDrawTermsAndConditions from "./QD/QuickDrawTermsAndConditions";
 import QuickDrawPrivacyPolicy from "./QD/QuickDrawPrivacyPolicy";
 import AutotokPrivacyPolicy from "./Autotok/AutotokPrivacyPolicy";
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/qd/pp" element={<QuickDrawPrivacyPolicy />} />
         <Route path="/qd/tc" element={<QuickDrawTermsAndConditions />} />
 
+        <Route path="/autotok" element={<Autotok />} />
         <Route path="/autotok/pp" element={<AutotokPrivacyPolicy />} />
         <Route path="/autotok/tos" element={<AutotokTermsOfService />} />
       </Routes>
---
+++ src/Autotok/Autotok.tsx
@@ -0,0 +1,26 @@
+import { Bolt } from "lucide-react";
+
+const Autotok = () => {
+  return (
+    <div className="h-screen">
+      <div className="flex text-2xl p-2 w-auto">
+        <Bolt size={32} /> <div className="w-2" />
+        Autotok <div className="ml-auto" />
+        <button title="Coming soon" disabled>
+          Login
+        </button>
+      </div>
+
+      <div className="h-1/4" />
+      <div className="flex">
+        <div className="w-1/5" />
+        <div className="flex flex-col gap-6">
+          <h1 className="text-6xl">Autotok</h1>
+          <h2 className="text-3xl">Generating content for you</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Autotok;
